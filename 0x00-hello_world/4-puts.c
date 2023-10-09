b33613127857658f46669207f33d15f5eebe6f63
goodness-uchechi <goodnessejiogu504@gmail.com>
2023-10-09 12:21:43 -0700

    task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- *  main - This is the main function
+ * main - This is the main function
  *
- *  Retun:  Always 0
+ * Return: Always 0
  */
 
 int main(void)
 {
-          puts("\"Programming is like building a multilingual puzzle\""):
-		return (0):  
+          puts("\"Programming is like building a multilingual puzzle\"");
+	  return (0);
 }	
