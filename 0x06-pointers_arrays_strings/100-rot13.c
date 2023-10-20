21f32c3ff76bf3e43f7f7722acfc7503b4a7f0dc
goodness-uchechi <goodnessejiogu504@gmail.com>
2023-10-20 03:55:23 -0700

    task 100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *rot13 - main function
+ *@s: string to be encoded
+ *Return: encoded string
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char *a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *b = "nopqrstuvwxyzabcdefghijklmNOPQRSTUWXYZABCDEFGHIJKLM";
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		for (j = 0 ; a[j] != '\0' ; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
