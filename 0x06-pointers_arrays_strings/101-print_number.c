bf356ceb54d53145b1ad1e6f7818496d29920afe
goodness-uchechi <goodnessejiogu504@gmail.com>
2023-10-21 08:37:16 -0700

    task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,20 +5,23 @@
  * @n: function parameter
  * Return: Always 0
  */
-
 void print_number(int n)
 {
-	unasigned int n1;
+	unsigned int n1;
 
 	if (n < 0)
 	{
-	_putchar('-');
-	n1 = -n;
+		_putchar('-');
+		n1 = -n;
+	}
+	else
+	{
+		n1 = n;
 	}
 
 	if (n1 / 10 != 0)
 	{
-	print_number(n1 / 10);
+		print_number(n1 / 10);
 	}
 	_putchar((n1 % 10) + '0');
 }
